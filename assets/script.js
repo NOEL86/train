ccafd134043b0c3feb655bd981498fc66569f0cf
Ashley <anoelj86@yahoo.com>
2018-06-01 22:23:45 -0600

    firebase logs

--- assets/script.js
+++ assets/script.js
@@ -13,6 +13,68 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 var name = "";
-var role = "";
-var date = "";
+var destination = "";
+var time = "";
 var rate = "";
+var nextTime = "";
+$("#button").on("click", function (event) {
+    event.preventDefault();
+
+    name = $("#name").val().trim();
+    destination = $("#destination").val().trim();
+    time = $("#time").val().trim();
+    rate = $("#rate").val().trim();
+    nextTime = $("#nextTime").val().trim();
+
+    $(".form-control").val("");
+
+    database.ref().push({
+        name: name,
+        destination: destination,
+        time: firebase.database.ServerValue.TIMESTAMP,
+        rate: rate,
+        nextTime: nextTime
+    });
+})
+
+database.ref().on("child_added", function (snapshot) {
+
+    var sv = snapshot.val();
+
+    getInfo();
+
+    console.log(sv.name);
+    console.log(sv.destination);
+    console.log(sv.time);
+    console.log(sv.rate);
+    console.log(sv.nextTime);
+
+    function getInfo() {
+        var trainTimes = $("#data-holder");
+
+        var newRow = $("<tr>");
+        var newName = $("<td>");
+        var newDestination = $("<td>");
+        var newTime = $("<td>");
+        var newRate = $("<td>");
+        var newMinutesAway = $("<td>");
+
+        trainTimes.append(newRow);
+        newRow.append(newName);
+        newRow.append(newDestination);
+        newRow.append(newTime);
+        newRow.append(newRate);
+        newRow.append(newMinutesAway);
+
+        newName.text(sv.name);
+        newDestination.text(sv.destination);
+        newTime.text(sv.time);
+        newRate.text(sv.rate);
+        newMinutesAway.text(sv.newMinutesAway);
+
+    }
+
+}, function (errorObject) {
+
+    console.log("Errors handled: " + errorObject.code);
+});
